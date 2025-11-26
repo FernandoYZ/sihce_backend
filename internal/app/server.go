a5b7597f0c0805742291712554b336b855a7a17a
FernandoYZ <fernandozavala266@gmail.com>
2025-11-26 11:20:49 -0500

    feat: añadir funciones de arranque y apagado del servidor HTTP

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,21 +1,26 @@
 package app
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"fmt"
+	"log"
+
 	"backend/internal/config"
 	"backend/internal/config/database"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type App struct {
-	Fiber  	*fiber.App
-	Config 	*config.Config
-	Db		*database.GestorDB
+	Fiber  *fiber.App
+	Config *config.Config
+	Db     *database.GestorDB
 }
 
 func New(cfg *config.Config, db *database.GestorDB) *App {
 	app := fiber.New(fiber.Config{
-		AppName:      "Backend API v1.0",
-		ErrorHandler: ErroresGlobales,
+		AppName:               "API Consulta Externa v3.0 - Backend",
+		ErrorHandler:          ErroresGlobales,
+		DisableStartupMessage: false,
 	})
 
 	ConfigurarMiddlewares(app, cfg)
@@ -26,4 +31,16 @@ func New(cfg *config.Config, db *database.GestorDB) *App {
 		Config: cfg,
 		Db:     db,
 	}
+}
+
+// Run inicia el servidor escuchando en el puerto configurado
+func (a *App) Run() error {
+	puerto := fmt.Sprintf(":%d", a.Config.App.Port)
+	return a.Fiber.Listen(puerto)
+}
+
+
+func (a *App) Shutdown() error {
+	log.Println("🛑 Apagando servidor HTTP...")
+	return a.Fiber.Shutdown()
 }
